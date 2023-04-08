8cb7a28c2173683e0839ccfb7dcf82eac88e716b
pranjaljaiswal35@gmail.com <pjpranjaljaiswal04@gmail.com>
2023-04-08 10:58:54 +0530

    refactor:optimize code

--- js/script.js
+++ js/script.js
@@ -1,46 +1,53 @@
-//Get Element 
+//Get form and form field
 let form = document.getElementById("formdetail");
 let fname = document.getElementById("fname");
 let email = document.getElementById("femail");
 let phone = document.getElementById("fphone");
 let password = document.getElementById("fpassword");
 let confirmp = document.getElementById("confirm");
-//Add event to the submit button
+//Add eventlistener to form submission
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  formvalidation();
+  formValidation();
 });
-//Display errormessage
-function errormessage(id, message) {
+//Display error message for field
+function errorMessage(id, message) {
   let span = id.nextElementSibling;
   span.innerHTML = message;
 }
-let istrue = true;
-function formvalidation() {
-  if (fname.value.length < 5)//Check the name field condition 
-  {
-    errormessage(fname, "*Length of name is too short");
-    istrue = false;
-  }
-  if (email.value.length < 15)//Check the email field condtion
-   {
-    errormessage(email, "*Email length is too long");
-    istrue = false;
-  }
-  if (phone.value.length != 10) //Check the phone no.field condtion
-  {
-    errormessage(phone, "*Phone number should be of 10 digit!");
-    istrue = false;
-  }
-  if (password.value.length < 6) //Check password field condition
-  {
-    errormessage(password, "*Password should be atleast 6 character long");
-    istrue = false;
-  }
-  if (password.value != confirmp.value) //Check confirm password field condtion
-  {
-    errormessage(confirmp, "*Password and Confirm password should match!");
-    istrue = false;
-  }
-}
 
+function formValidation() {
+  let isValid = true;
+
+  //Check name field condition
+  if (fname.value.length < 5) {
+    errorMessage(fname, "*Length of name is too short");
+    isValid = false;
+  }
+
+  //Check the email field condtion
+  if (email.value.length < 15) {
+    errorMessage(email, "*Email length is too long");
+    isValid = false;
+  }
+
+  //Check the phone no.field condtion
+  if (phone.value.length != 10) {
+    errorMessage(phone, "*Phone number should be of 10 digit!");
+    isValid = false;
+  }
+
+   //Check password field condition
+  if (password.value.length < 6) {
+    errorMessage(password, "*Password should be atleast 6 character long");
+    isValid = false;
+  }
+
+  //Check confirm password field condtion
+  if (password.value != confirmp.value) {
+    errorMessage(confirmp, "*Password and Confirm password should match!");
+    isValid = false;
+  }
+
+  return isValid;
+}
